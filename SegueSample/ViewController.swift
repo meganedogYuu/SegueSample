85771baffad2c75cf23f385e33c90a8268b00277
meganedogYuu <tkra.yuuki@gmail.com>
2016-06-04 00:43:56 +0900

    first commit

---
+++ SegueSample/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SegueSample
+//
+//  Created by Yuuki.Takara on 2016/05/27.
+//  Copyright © 2016年 Yuu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backToTop(segue: UIStoryboardSegue) {}
+}
+
